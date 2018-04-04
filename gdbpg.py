ac93e4d7d1a21df1864c8166663041ddfc56cd35
Taylor Vesely <tvesely@pivotal.io>
2018-04-04 09:52:18 -0700

    Add indent when returning "(NULL)" from various functions
    (cherry picked from commit a4b8d28b43cb359ba3f69d591d1608d36ae683a3)

--- gdbpg.py
+++ gdbpg.py
@@ -158,7 +158,7 @@ def format_plan_tree(tree, indent=0):
 def format_query_info(node, indent=0):
     'formats a query node with custom indentation'
     if (str(node) == '0x0'):
-        return '(NIL)'
+        return add_indent('(NULL)', indent)
 
     retval = '''          type: %(type)s
   command type: %(commandType)s
@@ -183,7 +183,7 @@ def format_query_info(node, indent=0):
         'returningList': format_node(node['returningList']),
       }
 
-    return retval
+    return add_indent(retval, 0)
 
 def format_appendplan_list(lst, indent):
     retval = format_node_list(lst, indent, True)
@@ -511,7 +511,7 @@ def format_node_list(lst, indent=0, newline=False):
 
     # handle NULL pointer (for List we return NIL)
     if (str(lst) == '0x0'):
-        return '(NIL)'
+        return add_indent('(NULL)', indent)
 
     # we'll collect the formatted items into a Python list
     tlist = []
